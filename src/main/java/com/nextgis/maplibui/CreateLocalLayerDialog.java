f082f3273c13d369258c4bfb0b91d830e86a9016
NikitaFeodonit <nikitafeodonit@yandex.com>
2015-02-21 12:31:04 +0100

    Cancel sync by account deleting

--- src/main/java/com/nextgis/maplibui/CreateLocalLayerDialog.java
+++ src/main/java/com/nextgis/maplibui/CreateLocalLayerDialog.java
@@ -29,6 +29,7 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.sqlite.SQLiteException;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -537,7 +538,7 @@ public class CreateLocalLayerDialog
                     }
                 }
             }
-        } catch (JSONException | IOException e) {
+        } catch (JSONException | IOException | SQLiteException e) {
             e.printStackTrace();
             return e.getLocalizedMessage();
         }
