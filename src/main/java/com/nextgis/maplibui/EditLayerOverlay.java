cc1fab938a462dcd534f6184f613474d45187a6a
Dmitry Baryshnikov <bishop.dev@gmail.com>
2015-03-04 00:10:47 +0300

    Add edit point menu

--- src/main/java/com/nextgis/maplibui/EditLayerOverlay.java
+++ src/main/java/com/nextgis/maplibui/EditLayerOverlay.java
@@ -23,18 +23,24 @@ package com.nextgis.maplibui;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PointF;
 import com.nextgis.maplib.datasource.GeoGeometry;
 import com.nextgis.maplib.datasource.GeoLineString;
+import com.nextgis.maplib.datasource.GeoMultiLineString;
 import com.nextgis.maplib.datasource.GeoMultiPoint;
+import com.nextgis.maplib.datasource.GeoMultiPolygon;
 import com.nextgis.maplib.datasource.GeoPoint;
+import com.nextgis.maplib.datasource.GeoPolygon;
 import com.nextgis.maplib.map.MapDrawable;
 import com.nextgis.maplib.map.VectorLayer;
+import com.nextgis.maplib.util.Constants;
 import com.nextgis.maplib.util.GeoConstants;
 import com.nextgis.maplib.util.VectorCacheItem;
 import com.nextgis.maplibui.api.Overlay;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -45,11 +51,21 @@ public class EditLayerOverlay
         extends Overlay
 {
     public final static int MODE_HIGHLIGHT = 1;
-    public final static int MODE_EDIT = 2;
+    public final static int MODE_EDIT      = 2;
+
+    protected final static int VERTEX_RADIUS = 20;
+    protected final static int EDGE_RADIUS = 12;
+    protected final static int LINE_WIDTH = 4;
+
     protected VectorLayer mLayer;
     protected VectorCacheItem mItem;
     protected int mMode;
     protected Paint mPaint;
+    protected int mFillColor;
+    protected int mOutlineColor;
+    protected int mSelectColor;
+    protected DrawItems mDrawItems;
+
 
     public EditLayerOverlay(
             Context context,
@@ -63,9 +79,14 @@ public class EditLayerOverlay
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setStrokeCap(Paint.Cap.ROUND);
-        mPaint.setColor(mContext.getResources().getColor(R.color.accent));
-        mPaint.setAlpha(190);
-        mPaint.setStrokeWidth(8);
+
+        mFillColor = mContext.getResources().getColor(R.color.accent);
+        mOutlineColor = Color.BLACK;
+        mSelectColor = Color.RED;
+
+        //mPaint.setAlpha(190);
+
+        mDrawItems = new DrawItems();
     }
 
 
@@ -75,61 +96,111 @@ public class EditLayerOverlay
     }
 
 
-    public void setFeature(VectorLayer layer, VectorCacheItem item){
+    public void setFeature(
+            VectorLayer layer,
+            VectorCacheItem item)
+    {
         mLayer = layer;
         mItem = item;
 
         mMapViewOverlays.postInvalidate();
     }
 
+
     @Override
     public void draw(
             Canvas canvas,
             MapDrawable mapDrawable)
     {
-        if(null == mItem)
+        if (null == mItem)
             return;
         GeoGeometry geom = mItem.getGeoGeometry();
-        if(null == geom)
+        if (null == geom)
             return;
+
+        fillDrawItems(geom, mapDrawable);
+
+        switch (geom.getType()){
+            case GeoConstants.GTPoint:
+            case GeoConstants.GTMultiPoint:
+                mDrawItems.drawPoints(canvas);
+                break;
+            case GeoConstants.GTLineString:
+            case GeoConstants.GTMultiLineString:
+            case GeoConstants.GTPolygon:
+            case GeoConstants.GTMultiPolygon:
+                mDrawItems.drawLines(canvas);
+                break;
+            default:
+                break;
+        }
+    }
+
+    protected void fillDrawItems(GeoGeometry geom, MapDrawable mapDrawable){
+        mDrawItems.clear();
 
         GeoPoint[] geoPoints = null;
         float[] points = null;
-        switch (geom.getType()){
+        GeoLineString lineString = null;
+        switch (geom.getType()) {
             case GeoConstants.GTPoint:
                 geoPoints = new GeoPoint[1];
                 geoPoints[0] = (GeoPoint) geom;
                 points = mapDrawable.mapToScreen(geoPoints);
-                mPaint.setStrokeWidth(30);
-                canvas.drawPoints(points, mPaint);
+                mDrawItems.addItems(0, points, DrawItems.TYPE_VERTEX);
                 break;
             case GeoConstants.GTMultiPoint:
                 GeoMultiPoint geoMultiPoint = (GeoMultiPoint) geom;
                 geoPoints = new GeoPoint[geoMultiPoint.size()];
-                for(int i = 0; i < geoMultiPoint.size(); i++){
+                for (int i = 0; i < geoMultiPoint.size(); i++) {
                     geoPoints[i] = geoMultiPoint.get(i);
                 }
                 points = mapDrawable.mapToScreen(geoPoints);
-                mPaint.setStrokeWidth(30);
-                canvas.drawPoints(points, mPaint);
+                mDrawItems.addItems(0, points, DrawItems.TYPE_VERTEX);
                 break;
             case GeoConstants.GTLineString:
-                GeoLineString lineString = (GeoLineString)geom;
-                geoPoints = lineString.getPoints().toArray(new GeoPoint[lineString.getPoints().size()]);
-                points = mapDrawable.mapToScreen(geoPoints);
-                mPaint.setStrokeWidth(10);
-                canvas.drawLines(points, mPaint);
+                lineString = (GeoLineString)geom;
+                fillDrawLine(0, lineString, mapDrawable);
                 break;
             case GeoConstants.GTMultiLineString:
+                GeoMultiLineString multiLineString = (GeoMultiLineString)geom;
+                for(int i = 0; i < multiLineString.size(); i++){
+                    fillDrawLine(i, multiLineString.get(i), mapDrawable);
+                }
+                break;
             case GeoConstants.GTPolygon:
+                GeoPolygon polygon = (GeoPolygon)geom;
+                fillDrawPolygon(polygon, mapDrawable);
+                break;
             case GeoConstants.GTMultiPolygon:
+                GeoMultiPolygon multiPolygon = (GeoMultiPolygon)geom;
+                for(int i = 0; i < multiPolygon.size(); i++){
+                    fillDrawPolygon(multiPolygon.get(i), mapDrawable);
+                }
+                break;
             case GeoConstants.GTGeometryCollection:
+            default:
                 break;
         }
-
-
     }
 
+    protected void fillDrawPolygon(GeoPolygon polygon, MapDrawable mapDrawable){
+        fillDrawLine(0, polygon.getOuterRing(), mapDrawable);
+        for(int i = 0; i < polygon.getInnerRingCount(); i++){
+            fillDrawLine(i + 1, polygon.getInnerRing(i), mapDrawable);
+        }
+    }
+
+    protected void fillDrawLine(int ring, GeoLineString lineString, MapDrawable mapDrawable){
+        GeoPoint[] geoPoints = lineString.getPoints().toArray(new GeoPoint[lineString.getPoints().size()]);
+        float[] points = mapDrawable.mapToScreen(geoPoints);
+        mDrawItems.addItems(ring, points, DrawItems.TYPE_VERTEX);
+        float[] edgePoints = new float[points.length - 1];
+        for(int i = 0; i < points.length - 1; i++){
+            edgePoints[i] = (points[i] + points[i + 1]) * .5f;
+        }
+        mDrawItems.addItems(ring, edgePoints, DrawItems.TYPE_EDGE);
+    }
 
     @Override
     public void drawOnPanning(
@@ -157,4 +228,126 @@ public class EditLayerOverlay
             return;
     }
 
+    protected class DrawItems{
+        List<float[]> mDrawItemsVertex;
+        List<float[]> mDrawItemsEdge;
+
+        public static final int TYPE_VERTEX = 1;
+        public static final int TYPE_EDGE   = 2;
+
+        protected int mSelectedRing, mSelectedPoint;
+
+
+        public DrawItems()
+        {
+            mDrawItemsVertex = new ArrayList<>();
+            mDrawItemsEdge = new ArrayList<>();
+
+            mSelectedRing = Constants.NOT_FOUND;
+            mSelectedPoint = Constants.NOT_FOUND;
+        }
+
+
+        public void setSelectedRing(int selectedRing)
+        {
+            mSelectedRing = selectedRing;
+        }
+
+
+        public void setSelectedPoint(int selectedPoint)
+        {
+            mSelectedPoint = selectedPoint;
+        }
+
+
+        public void addItems(
+                int ring,
+                float[] points,
+                int type)
+        {
+            if(type == TYPE_VERTEX)
+                mDrawItemsVertex.add(ring, points);
+            else if(type == TYPE_EDGE)
+                mDrawItemsEdge.add(ring, points);
+        }
+
+
+        public void clear()
+        {
+            mDrawItemsVertex.clear();
+            mDrawItemsEdge.clear();
+        }
+
+
+        public void drawPoints(Canvas canvas)
+        {
+            for (float[] items : mDrawItemsVertex) {
+
+                mPaint.setColor(mOutlineColor);
+                mPaint.setStrokeWidth(VERTEX_RADIUS + 2);
+                canvas.drawPoints(items, mPaint);
+
+                mPaint.setColor(mFillColor);
+                mPaint.setStrokeWidth(VERTEX_RADIUS);
+                canvas.drawPoints(items, mPaint);
+            }
+
+            //draw selected point
+            if(mSelectedRing != Constants.NOT_FOUND && mSelectedPoint != Constants.NOT_FOUND) {
+
+                float[] items = mDrawItemsVertex.get(mSelectedRing);
+                if(null != items) {
+                    mPaint.setColor(mSelectColor);
+                    mPaint.setStrokeWidth(VERTEX_RADIUS);
+
+                    canvas.drawPoint(items[mSelectedPoint], items[mSelectedPoint + 1], mPaint);
+                }
+            }
+        }
+
+        public void drawLines(Canvas canvas)
+        {
+            for (float[] items : mDrawItemsVertex) {
+
+                mPaint.setColor(mFillColor);
+                mPaint.setStrokeWidth(LINE_WIDTH);
+                canvas.drawLines(items, mPaint);
+
+                if(mMode == MODE_EDIT) {
+                    mPaint.setColor(mOutlineColor);
+                    mPaint.setStrokeWidth(VERTEX_RADIUS + 2);
+                    canvas.drawPoints(items, mPaint);
+
+                    mPaint.setColor(mFillColor);
+                    mPaint.setStrokeWidth(VERTEX_RADIUS);
+                    canvas.drawPoints(items, mPaint);
+                }
+            }
+
+            if(mMode == MODE_EDIT) {
+                for (float[] items : mDrawItemsEdge) {
+
+                    mPaint.setColor(mOutlineColor);
+                    mPaint.setStrokeWidth(EDGE_RADIUS + 2);
+                    canvas.drawPoints(items, mPaint);
+
+                    mPaint.setColor(mFillColor);
+                    mPaint.setStrokeWidth(EDGE_RADIUS);
+                    canvas.drawPoints(items, mPaint);
+                }
+            }
+
+            //draw selected point
+            if(mSelectedRing != Constants.NOT_FOUND && mSelectedPoint != Constants.NOT_FOUND) {
+
+                float[] items = mDrawItemsVertex.get(mSelectedRing);
+                if(null != items) {
+                    mPaint.setColor(mSelectColor);
+                    mPaint.setStrokeWidth(VERTEX_RADIUS);
+
+                    canvas.drawPoint(items[mSelectedPoint], items[mSelectedPoint + 1], mPaint);
+                }
+            }
+        }
+    }
 }
