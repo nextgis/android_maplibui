7249926a39b812aeb7a9f71bf802a092d666cb3f
BishopGIS <bishop.dev@gmail.com>
2015-08-15 21:50:06 +0300

    Add sign control

--- src/main/java/com/nextgis/maplibui/formcontrol/Sign.java
+++ src/main/java/com/nextgis/maplibui/formcontrol/Sign.java
@@ -24,17 +24,27 @@ package com.nextgis.maplibui.formcontrol;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.util.AttributeSet;
-import android.util.Log;
-import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 
-import com.nextgis.maplib.util.Constants;
 import com.nextgis.maplibui.R;
+import com.nextgis.maplibui.util.SettingsConstantsUI;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.LinkedList;
 
 /**
  * A special control for create sign picture.
@@ -44,9 +54,14 @@ public class Sign extends View {
     protected Drawable mCleanImage;
     protected int mClearBuff;
     protected int mClearImageSize;
+    protected Path    mPath;
+    protected final LinkedList<Path> mPaths = new LinkedList<>();
+    protected Paint mPaint;
+    protected float mX, mY;
 
     protected final int CLEAR_BUFF_DP = 15;
     protected final int CLEAR_IMAGE_SIZE_DP = 32;
+    protected static final float TOUCH_TOLERANCE = 4;
 
     public Sign(Context context) {
         super(context);
@@ -78,6 +93,23 @@ public class Sign extends View {
 
         mClearBuff = (int) (getContext().getResources().getDisplayMetrics().density * CLEAR_BUFF_DP);
         mClearImageSize = (int) (getContext().getResources().getDisplayMetrics().density * CLEAR_IMAGE_SIZE_DP);
+
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(3);
+        mPaint.setDither(true);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+
+        boolean bDark = PreferenceManager.getDefaultSharedPreferences(getContext())
+                .getString(SettingsConstantsUI.KEY_PREF_THEME, "light").equals("dark");
+        if(bDark)
+            mPaint.setColor(Color.WHITE);
+        else
+            mPaint.setColor(Color.BLACK);
+
+        mPath = new Path();
+        mPaths.add(mPath);
     }
 
     @Override
@@ -85,32 +117,74 @@ public class Sign extends View {
     {
         int posX = canvas.getWidth() - mClearImageSize - mClearBuff;
 
-        mCleanImage.setBounds(posX,  mClearBuff, posX + mClearImageSize, mClearImageSize + mClearBuff);
+        mCleanImage.setBounds(posX, mClearBuff, posX + mClearImageSize, mClearImageSize + mClearBuff);
         mCleanImage.draw(canvas);
 
-        /*
-        if (mMap != null) {
-            mMap.draw(canvas, 0, 0, false);
-        } else {
-            super.onDraw(canvas);
-        }*/
+        for (Path path : mPaths){
+            canvas.drawPath(path, mPaint);
+        }
+    }
+
+    protected void drawSign(Canvas canvas, int bkColor, Paint paint){
+        canvas.drawColor(bkColor);
+
+        for (Path path : mPaths){
+            canvas.drawPath(path, paint);
+        }
+    }
+
+    protected void touchStart(float x, float y) {
+        mPath.reset();
+        mPath.moveTo(x, y);
+        mX = x;
+        mY = y;
+    }
+
+    protected void touchMove(float x, float y) {
+        float dx = Math.abs(x - mX);
+        float dy = Math.abs(y - mY);
+        if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+            mPath.quadTo(mX, mY, (x + mX)/2, (y + mY)/2);
+            mX = x;
+            mY = y;
+        }
+    }
+
+    protected void touchUp() {
+        mPath.lineTo(mX, mY);
+
+        postInvalidate();
+
+        // kill this so we don't double draw
+        mPath = new Path();
+        mPaths.add(mPath);
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
+    public boolean onTouchEvent(@NonNull MotionEvent event) {
+
+        float x = event.getX();
+        float y = event.getY();
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
+                touchStart(x, y);
                 break;
 
             case MotionEvent.ACTION_MOVE:
+                touchMove(x, y);
+                postInvalidate();
                 break;
 
             case MotionEvent.ACTION_UP:
                 int posX = getWidth() - mClearImageSize - mClearBuff;
-                Log.d(Constants.TAG, "x: " + event.getX() + " y: " + event.getY() + " posX: " + posX + " posY: " + mClearBuff);
+                //Log.d(Constants.TAG, "x: " + event.getX() + " y: " + event.getY() + " posX: " + posX + " posY: " + mClearBuff);
                 if(event.getX() > posX && event.getY() < mClearImageSize + mClearBuff){
                     onClearSign();
+                }
+                else{
+                    touchUp();
+                    invalidate();
                 }
                 break;
 
@@ -122,6 +196,34 @@ public class Sign extends View {
     }
 
     private void onClearSign() {
-        int x = 0;
+        mPaths.clear();
+        mPath = new Path();
+        mPaths.add(mPath);
+
+        postInvalidate();
+    }
+
+    public void save(int width, int height, boolean transparentBackground, File sigFile) throws IOException {
+        float scale = Math.min((float) width / getWidth(), (float) height / getHeight());
+        Matrix matrix = new Matrix();
+        matrix.setScale(scale, scale);
+
+        Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bmp);
+        canvas.setMatrix(matrix);
+
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(3);
+        paint.setDither(true);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        paint.setColor(Color.BLACK);
+
+        drawSign(canvas, Color.argb(transparentBackground ? 0 : 255, 255, 255, 255), paint);
+        if(sigFile.exists() || sigFile.createNewFile()) {
+            FileOutputStream out = new FileOutputStream(sigFile);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+        }
     }
 }
