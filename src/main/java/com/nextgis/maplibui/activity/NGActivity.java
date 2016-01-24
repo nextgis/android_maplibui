35b8d3a736283fd874e12e9d56a561361d574f2a
NikitaFeodonit <nikitafeodonit@yandex.com>
2016-01-24 03:12:35 +0300

    Some refactoring

--- src/main/java/com/nextgis/maplibui/activity/NGActivity.java
+++ src/main/java/com/nextgis/maplibui/activity/NGActivity.java
@@ -2,6 +2,7 @@
  * Project:  NextGIS Mobile
  * Purpose:  Mobile GIS for Android.
  * Author:   Dmitry Baryshnikov (aka Bishop), bishop.dev@gmail.com
+ * Author:   NikitaFeodonit, nfeodonit@yandex.com
  * *****************************************************************************
  * Copyright (c) 2012-2015. NextGIS, info@nextgis.com
  *
@@ -28,53 +29,83 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-
 import com.nextgis.maplibui.R;
 import com.nextgis.maplibui.util.SettingsConstantsUI;
+
 
 /**
  * A base activity for NextGIS derived activities
  */
 public class NGActivity
-        extends AppCompatActivity {
+        extends AppCompatActivity
+{
     protected final static String KEY_CURRENT_THEME = "current_theme";
 
     protected SharedPreferences mPreferences;
-    protected boolean mIsDarkTheme;
+    protected boolean           mIsDarkTheme;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        setTheme();
+        mIsDarkTheme =
+                mPreferences.getString(SettingsConstantsUI.KEY_PREF_THEME, "light").equals("dark");
+        setCurrentThemePref();
+        setTheme(getThemeId());
         super.onCreate(savedInstanceState);
     }
 
-    protected void setTheme(){
-        mIsDarkTheme = mPreferences.getString(SettingsConstantsUI.KEY_PREF_THEME, "light").equals("dark");
-        mPreferences.edit().putString(KEY_CURRENT_THEME, mIsDarkTheme ? "dark" : "light").commit();
-        setTheme(getThemeId());
+
+    public int getThemeId()
+    {
+        return mIsDarkTheme
+               ? R.style.Theme_NextGIS_AppCompat_Dark
+               : R.style.Theme_NextGIS_AppCompat_Light;
     }
+
+
+    // for overriding in a subclass
+    protected void setCurrentThemePref()
+    {
+        mPreferences.edit().putString(KEY_CURRENT_THEME, mIsDarkTheme ? "dark" : "light").commit();
+    }
+
+
+    // for overriding in a subclass
+    protected void refreshCurrentTheme()
+    {
+        String newTheme = mPreferences.getString(SettingsConstantsUI.KEY_PREF_THEME, "light");
+        String currentTheme = mPreferences.getString(KEY_CURRENT_THEME, "light");
+
+        if (!newTheme.equals(currentTheme)) {
+            refreshActivityView();
+        }
+    }
+
+
     /**
-     * This hook is called whenever an item in your options menu is selected.
-     * The default implementation simply returns false to have the normal
-     * processing happen (calling the item's Runnable or sending a message to
-     * its Handler as appropriate).  You can use this method for any items
-     * for which you would like to do processing without those other
-     * facilities.
-     * <p/>
-     * <p>Derived classes should call through to the base class for it to
-     * perform the default menu handling.</p>
+     * This hook is called whenever an item in your options menu is selected. The default
+     * implementation simply returns false to have the normal processing happen (calling the item's
+     * Runnable or sending a message to its Handler as appropriate).  You can use this method for
+     * any items for which you would like to do processing without those other facilities. <p/>
+     * <p>Derived classes should call through to the base class for it to perform the default menu
+     * handling.</p>
      *
-     * @param item The menu item that was selected.
-     * @return boolean Return false to allow normal menu processing to
-     * proceed, true to consume it here.
+     * @param item
+     *         The menu item that was selected.
+     *
+     * @return boolean Return false to allow normal menu processing to proceed, true to consume it
+     * here.
+     *
      * @see #onCreateOptionsMenu
      */
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         switch (item.getItemId()) {
             case android.R.id.home:
-                if(isHomeEnabled()) {
+                if (isHomeEnabled()) {
                     finish();
                     return true;
                 }
@@ -90,10 +121,12 @@ public class NGActivity
     }
 
 
-    protected void setToolbar(int toolbarId){
+    protected void setToolbar(int toolbarId)
+    {
         Toolbar toolbar = (Toolbar) findViewById(toolbarId);
-        if(null == toolbar)
+        if (null == toolbar) {
             return;
+        }
         toolbar.getBackground().setAlpha(getToolbarAlpha());
         setSupportActionBar(toolbar);
 
@@ -103,34 +136,25 @@ public class NGActivity
         }
     }
 
-    protected int getToolbarAlpha(){
+
+    protected int getToolbarAlpha()
+    {
         return 255; // not transparent
     }
 
-    public int getThemeId(){
-        return mIsDarkTheme ? R.style.Theme_NextGIS_AppCompat_Dark : R.style.Theme_NextGIS_AppCompat_Light;
-    }
 
     @Override
-    protected void onResume() {
-        String newTheme = mPreferences.getString(SettingsConstantsUI.KEY_PREF_THEME, "light");
-        String currentTheme = mPreferences.getString(KEY_CURRENT_THEME, "light");
-
-        if (!newTheme.equals(currentTheme))
-            refreshActivityView();
-
+    protected void onResume()
+    {
+        refreshCurrentTheme();
         super.onResume();
     }
+
 
     public void refreshActivityView()
     {
         Intent intent = getIntent();
         finish();
         startActivity(intent);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
     }
 }
