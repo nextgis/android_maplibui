686bd91c5341ff1cef3420e0c8c4c0faebe810fb
Stanislav Petryakov <become.glory@yandex.ru>
2016-09-01 23:28:38 +0300

    Fetch NGW version, #412

--- src/main/java/com/nextgis/maplibui/util/NGWCreateNewLayerTask.java
+++ src/main/java/com/nextgis/maplibui/util/NGWCreateNewLayerTask.java
@@ -23,30 +23,53 @@ package com.nextgis.maplibui.util;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Pair;
 import android.widget.Toast;
 
 import com.nextgis.maplib.datasource.ngw.Connection;
 import com.nextgis.maplib.map.VectorLayer;
+import com.nextgis.maplib.util.AccountUtil;
 import com.nextgis.maplib.util.Constants;
+import com.nextgis.maplib.util.NGException;
 import com.nextgis.maplib.util.NGWUtil;
 import com.nextgis.maplibui.R;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+
 public class NGWCreateNewLayerTask extends AsyncTask<Void, Void, String> {
     private Connection mConnection;
     private VectorLayer mLayer;
+    private Context mContext;
+    private Pair<Integer, Integer> mVer;
 
     public NGWCreateNewLayerTask(Connection connection, VectorLayer layer) {
         mConnection = connection;
         mLayer = layer;
+        mContext = mLayer.getContext();
     }
 
     @Override
     protected String doInBackground(Void... voids) {
-        if (mConnection.connect())
+        if (mConnection.connect()) {
+            mVer = null;
+            try {
+                AccountUtil.AccountData accountData = AccountUtil.getAccountData(mContext, mConnection.getName());
+
+                if (null == accountData || null == accountData.url)
+                    return null;
+
+                mVer = NGWUtil.getNgwVersion(accountData.url, accountData.login, accountData.password);
+            } catch (IllegalStateException e) {
+                return null;
+            } catch (IOException | NGException | NumberFormatException ignored) { } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
             return NGWUtil.createNewLayer(mConnection, mLayer);
+        }
 
         return null;
     }
@@ -55,20 +78,19 @@ public class NGWCreateNewLayerTask extends AsyncTask<Void, Void, String> {
     protected void onPostExecute(String result) {
         super.onPostExecute(result);
 
-        Context context = mLayer.getContext();
-        String message = context.getString(R.string.error_layer_create);
+        String message = mContext.getString(R.string.error_layer_create);
         if (result != null) {
             try {
                 JSONObject obj = new JSONObject(result);
                 message = obj.optString(Constants.JSON_MESSAGE_KEY, message);
                 Long id = obj.getLong(Constants.JSON_ID_KEY);
-                mLayer.toNGW(id, mConnection.getName());
-                message = context.getString(R.string.message_layer_created);
+                mLayer.toNGW(id, mConnection.getName(), mVer);
+                message = mContext.getString(R.string.message_layer_created);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
 
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
     }
 }
