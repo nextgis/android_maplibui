98890a827b47e1db795290247515bc75f5ebf14c
NikitaFeodonit <nikitafeodonit@yandex.com>
2017-03-22 18:03:41 +0300

    Add getTintedDrawable() to ControlHelper

--- src/main/java/com/nextgis/maplibui/util/ControlHelper.java
+++ src/main/java/com/nextgis/maplibui/util/ControlHelper.java
@@ -41,6 +41,8 @@ import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.ColorInt;
+import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -81,45 +83,74 @@ import static com.nextgis.maplib.util.GeoConstants.GTPolygon;
 import static com.nextgis.maplibui.util.ConstantsUI.JSON_INPUT_SEARCH;
 import static com.nextgis.maplibui.util.ConstantsUI.JSON_SHOW_LAST_KEY;
 
-public final class ControlHelper {
+
+public final class ControlHelper
+{
     private final static String BUNDLE_SAVED_STATE = "nextgis_control_";
 
-    public static String getPercentValue(Context context, int stringLabel, float value) {
+    public static String getPercentValue(
+            Context context,
+            int stringLabel,
+            float value)
+    {
         return context.getString(stringLabel) + ": " + ((int) value * 100 / 255) + "%";
     }
 
-    public static void setEnabled(MenuItem item, boolean state) {
-        if(null == item)
+    public static void setEnabled(
+            MenuItem item,
+            boolean state)
+    {
+        if (null == item) {
             return;
+        }
         item.setEnabled(state);
         item.getIcon().setAlpha(state ? 255 : 160);
     }
 
-    public static boolean isEnabled(List<Field> fields, String fieldName) {
-        for (Field field : fields)
-            if (field.getName().equals(fieldName))
+    public static boolean isEnabled(
+            List<Field> fields,
+            String fieldName)
+    {
+        for (Field field : fields) {
+            if (field.getName().equals(fieldName)) {
                 return true;
+            }
+        }
 
         return false;
     }
 
-    public static boolean isSaveLastValue(JSONObject attributes) throws JSONException {
+    public static boolean isSaveLastValue(JSONObject attributes)
+            throws JSONException
+    {
         return !attributes.isNull(JSON_SHOW_LAST_KEY) && attributes.getBoolean(JSON_SHOW_LAST_KEY);
     }
 
-    public static boolean isAutoComplete(JSONObject attributes) throws JSONException {
+    public static boolean isAutoComplete(JSONObject attributes)
+            throws JSONException
+    {
         return attributes.has(JSON_INPUT_SEARCH) && attributes.getBoolean(JSON_INPUT_SEARCH);
     }
 
-    public static boolean hasKey(Bundle savedState, String fieldName) {
+    public static boolean hasKey(
+            Bundle savedState,
+            String fieldName)
+    {
         return savedState != null && savedState.containsKey(getSavedStateKey(fieldName));
     }
 
-    public static String getSavedStateKey(String fieldName) {
+    public static String getSavedStateKey(String fieldName)
+    {
         return BUNDLE_SAVED_STATE + fieldName;
     }
 
-    public static Drawable getIconByVectorType(Context context, int geometryType, int color, int defaultIcon, boolean syncable) {
+    public static Drawable getIconByVectorType(
+            Context context,
+            int geometryType,
+            int color,
+            int defaultIcon,
+            boolean syncable)
+    {
         int drawableId;
 
         switch (geometryType) {
@@ -157,10 +188,15 @@ public final class ControlHelper {
             canvas.drawBitmap(src, 0, 0, paint);
 
             if (syncable) {
-                int syncIconId = isDarkTheme(context) ? R.drawable.ic_action_refresh_dark : R.drawable.ic_action_refresh_light;;
+                int syncIconId = isDarkTheme(context)
+                                 ? R.drawable.ic_action_refresh_dark
+                                 : R.drawable.ic_action_refresh_light;
+                ;
                 src = BitmapFactory.decodeResource(context.getResources(), syncIconId);
-                src = Bitmap.createScaledBitmap(src, bitmap.getWidth() / 2, bitmap.getWidth() / 2, true);
-                canvas.drawBitmap(src, bitmap.getWidth() - bitmap.getWidth() / 2, bitmap.getWidth() - bitmap.getWidth() / 2, new Paint());
+                src = Bitmap.createScaledBitmap(src, bitmap.getWidth() / 2, bitmap.getWidth() / 2,
+                        true);
+                canvas.drawBitmap(src, bitmap.getWidth() - bitmap.getWidth() / 2,
+                        bitmap.getWidth() - bitmap.getWidth() / 2, new Paint());
             }
 
             icon = new BitmapDrawable(context.getResources(), bitmap);
@@ -169,20 +205,30 @@ public final class ControlHelper {
         return icon;
     }
 
-    public static boolean isDarkTheme(Context context) {
-        return PreferenceManager.getDefaultSharedPreferences(context).getString(SettingsConstantsUI.KEY_PREF_THEME, "light").equals("dark");
+    public static boolean isDarkTheme(Context context)
+    {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(SettingsConstantsUI.KEY_PREF_THEME, "light")
+                .equals("dark");
     }
 
-    public static Drawable tintDrawable(Drawable drawable, int color) {
-        if (drawable == null)
+    public static Drawable tintDrawable(
+            Drawable drawable,
+            int color)
+    {
+        if (drawable == null) {
             return null;
+        }
 
         drawable = DrawableCompat.wrap(drawable);
         DrawableCompat.setTint(drawable, color);
         return drawable;
     }
 
-    public static int getDialogTheme(Context context, int theme) {
+    public static int getDialogTheme(
+            Context context,
+            int theme)
+    {
         int dialogTheme = R.style.Theme_NextGIS_AppCompat_Light_Dialog;
         int[] attrs = {android.R.attr.alertDialogStyle};
         TypedArray ta = context.obtainStyledAttributes(theme, attrs);
@@ -192,17 +238,26 @@ public final class ControlHelper {
         return dialogTheme;
     }
 
-    public static String getSyncTime(Context context, long timeStamp) {
-        String date = new SimpleDateFormat("dd MMM", Locale.getDefault()).format(new Date(timeStamp));
-        String time = new SimpleDateFormat("HH:mm", Locale.getDefault()).format(new Date(timeStamp));
+    public static String getSyncTime(
+            Context context,
+            long timeStamp)
+    {
+        String date =
+                new SimpleDateFormat("dd MMM", Locale.getDefault()).format(new Date(timeStamp));
+        String time =
+                new SimpleDateFormat("HH:mm", Locale.getDefault()).format(new Date(timeStamp));
         return String.format(context.getString(R.string.last_sync_time), date, time);
     }
 
 
-    public static Bitmap getBitmap(InputStream is, BitmapFactory.Options options) {
+    public static Bitmap getBitmap(
+            InputStream is,
+            BitmapFactory.Options options)
+    {
         Bitmap result = null;
-        if (is == null)
+        if (is == null) {
             return null;
+        }
 
         try {
             result = BitmapFactory.decodeStream(is, null, options);
@@ -227,18 +282,24 @@ public final class ControlHelper {
     }
 
     @SuppressWarnings("deprecation")
-    public static BitmapFactory.Options getOptions(InputStream is, int width, int height) {
-        if (is == null)
+    public static BitmapFactory.Options getOptions(
+            InputStream is,
+            int width,
+            int height)
+    {
+        if (is == null) {
             return null;
+        }
 
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = true;
         BitmapFactory.decodeStream(is, null, options);
 
-        if (height == 0)
+        if (height == 0) {
             height = (int) ((1f * width * options.outHeight / options.outWidth));
-        else if (width == 0)
+        } else if (width == 0) {
             width = (int) ((1f * height * options.outWidth / options.outHeight));
+        }
 
         options.inSampleSize = calculateInSampleSize(options, width, height);
         options.inJustDecodeBounds = false;
@@ -249,7 +310,11 @@ public final class ControlHelper {
         return options;
     }
 
-    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options,
+            int reqWidth,
+            int reqHeight)
+    {
         final int height = options.outHeight;
         final int width = options.outWidth;
         int inSampleSize = 1;
@@ -267,53 +332,63 @@ public final class ControlHelper {
         return inSampleSize;
     }
 
-    public static int getColor(Context context, int attr) {
+    public static int getColor(
+            Context context,
+            int attr)
+    {
         TypedValue typedValue = new TypedValue();
         Resources.Theme theme = context.getTheme();
         theme.resolveAttribute(attr, typedValue, true);
         return typedValue.data;
     }
 
-    public static int dpToPx(int dp, Resources resources) {
+    public static int dpToPx(
+            int dp,
+            Resources resources)
+    {
         DisplayMetrics dm = resources.getDisplayMetrics();
         return Math.round(dp * (dm.xdpi / DisplayMetrics.DENSITY_DEFAULT));
     }
 
     // http://stackoverflow.com/a/32973351/2088273
-    public static AppCompatActivity getActivity(Context context) {
+    public static AppCompatActivity getActivity(Context context)
+    {
         while (context instanceof ContextWrapper) {
-            if (context instanceof AppCompatActivity)
+            if (context instanceof AppCompatActivity) {
                 return (AppCompatActivity) context;
+            }
 
-            context = ((ContextWrapper)context).getBaseContext();
+            context = ((ContextWrapper) context).getBaseContext();
         }
 
         return null;
     }
 
     @TargetApi(Build.VERSION_CODES.GINGERBREAD)
-    public static void lockScreenOrientation(Activity activity) {
-        WindowManager windowManager =  (WindowManager) activity.getSystemService(Context.WINDOW_SERVICE);
+    public static void lockScreenOrientation(Activity activity)
+    {
+        WindowManager windowManager =
+                (WindowManager) activity.getSystemService(Context.WINDOW_SERVICE);
         Configuration configuration = activity.getResources().getConfiguration();
         int rotation = windowManager.getDefaultDisplay().getRotation();
 
         // Search for the natural position of the device
-        if(configuration.orientation == Configuration.ORIENTATION_LANDSCAPE &&
-                (rotation == Surface.ROTATION_0 || rotation == Surface.ROTATION_180) ||
-                configuration.orientation == Configuration.ORIENTATION_PORTRAIT &&
-                        (rotation == Surface.ROTATION_90 || rotation == Surface.ROTATION_270))
-        {
+        if (configuration.orientation == Configuration.ORIENTATION_LANDSCAPE && (
+                rotation == Surface.ROTATION_0 || rotation == Surface.ROTATION_180)
+                || configuration.orientation == Configuration.ORIENTATION_PORTRAIT && (
+                rotation == Surface.ROTATION_90 || rotation == Surface.ROTATION_270)) {
             // Natural position is Landscape
-            switch (rotation)
-            {
+            switch (rotation) {
                 case Surface.ROTATION_0:
                     activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
                     break;
                 case Surface.ROTATION_90:
-                    activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT);
+                    activity.setRequestedOrientation(
+                            ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT);
                     break;
                 case Surface.ROTATION_180:
-                    activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+                    activity.setRequestedOrientation(
+                            ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
                     break;
                 case Surface.ROTATION_270:
                     activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
@@ -321,8 +396,7 @@ public final class ControlHelper {
             }
         } else {
             // Natural position is Portrait
-            switch (rotation)
-            {
+            switch (rotation) {
                 case Surface.ROTATION_0:
                     activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
                     break;
@@ -330,26 +404,37 @@ public final class ControlHelper {
                     activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
                     break;
                 case Surface.ROTATION_180:
-                    activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT);
+                    activity.setRequestedOrientation(
+                            ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT);
                     break;
                 case Surface.ROTATION_270:
-                    activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+                    activity.setRequestedOrientation(
+                            ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
                     break;
             }
         }
     }
 
-    public static void unlockScreenOrientation(Activity activity) {
+    public static void unlockScreenOrientation(Activity activity)
+    {
         activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
     }
 
-    public static void setClearAction(final EditText target) {
-        target.setOnTouchListener(new View.OnTouchListener() {
+    public static void setClearAction(final EditText target)
+    {
+        target.setOnTouchListener(new View.OnTouchListener()
+        {
             final int RIGHT = 2;
+
             @Override
-            public boolean onTouch(View view, MotionEvent event) {
+            public boolean onTouch(
+                    View view,
+                    MotionEvent event)
+            {
                 if (event.getAction() == MotionEvent.ACTION_UP) {
-                    int leftEdgeOfRightDrawable = view.getRight() - target.getCompoundDrawables()[RIGHT].getBounds().width();
+                    int leftEdgeOfRightDrawable =
+                            view.getRight() - target.getCompoundDrawables()[RIGHT].getBounds()
+                                    .width();
                     // when EditBox has padding, adjust leftEdge like
                     // leftEdgeOfRightDrawable -= getResources().getDimension(R.dimen.edittext_padding_left_right);
                     if (event.getRawX() >= leftEdgeOfRightDrawable) {
@@ -364,16 +449,44 @@ public final class ControlHelper {
         });
     }
 
-    public static void setZoomText(Activity activity, TextView text, int string, int zoom) {
+    public static void setZoomText(
+            Activity activity,
+            TextView text,
+            int string,
+            int zoom)
+    {
         String scale = LocationUtil.formatLength(activity, MapUtil.getScaleInCm(activity, zoom), 0);
         text.setText(String.format(activity.getString(string) + "\r\n" + scale, zoom));
     }
 
-    public static void highlightText(TextView textView) {
+    public static void highlightText(TextView textView)
+    {
         final CharSequence text = textView.getText();
         final SpannableString spannableString = new SpannableString(text);
-        spannableString.setSpan(new URLSpan(""), 0, spannableString.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(
+                new URLSpan(""), 0, spannableString.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         textView.setText(spannableString, TextView.BufferType.SPANNABLE);
     }
 
+    /**
+     * Specifies a tint for {@code inputDrawable}.
+     *
+     * @param inputDrawable
+     *         The Drawable which to apply the tint.
+     * @param tint
+     *         Color to use for tinting returned drawable.
+     *
+     * @return Wrapped drawable with {@link android.support.v4.graphics.drawable.DrawableCompat#wrap(Drawable)}
+     */
+    public static Drawable getTintedDrawable(
+            @NonNull
+                    Drawable inputDrawable,
+            @ColorInt
+                    int tint)
+    {
+        Drawable wrapDrawable = DrawableCompat.wrap(inputDrawable);
+        DrawableCompat.setTint(wrapDrawable, tint);
+//        DrawableCompat.setTintMode(wrapDrawable, PorterDuff.Mode.SRC_IN);
+        return wrapDrawable;
+    }
 }
