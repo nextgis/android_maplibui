98989a75f2c560e28a02bd76085516f16debc04f
NikitaFeodonit <nikitafeodonit@yandex.com>
2017-04-03 19:53:33 +0300

    Add GridAutofitLayoutManager

---
+++ src/main/java/com/nextgis/maplibui/adapter/GridAutofitLayoutManager.java
@@ -0,0 +1,112 @@
+/*
+ * Project:  NextGIS Mobile
+ * Purpose:  Mobile GIS for Android.
+ * Author:   Dmitry Baryshnikov (aka Bishop), bishop.dev@gmail.com
+ * Author:   NikitaFeodonit, nfeodonit@yandex.com
+ * Author:   Stanislav Petriakov, becomeglory@gmail.com
+ * *****************************************************************************
+ * Copyright (c) 2012-2017 NextGIS, info@nextgis.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.nextgis.maplibui.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
+
+
+// From http://stackoverflow.com/a/30256880
+public class GridAutofitLayoutManager
+        extends GridLayoutManager
+{
+    protected float mColumnWidth;
+    protected boolean mColumnWidthChanged = true;
+
+    protected int mWidth;
+    protected boolean mWidthChanged = true;
+
+    public GridAutofitLayoutManager(
+            Context context,
+            float columnWidthDp)
+    {
+        // Initially set spanCount to 1, will be changed automatically later.
+        super(context, 1);
+        setColumnWidth(checkedColumnWidth(context, columnWidthDp));
+    }
+
+    public GridAutofitLayoutManager(
+            Context context,
+            float columnWidth,
+            int orientation,
+            boolean reverseLayout)
+    {
+        // Initially set spanCount to 1, will be changed automatically later.
+        super(context, 1, orientation, reverseLayout);
+        setColumnWidth(checkedColumnWidth(context, columnWidth));
+    }
+
+    protected float checkedColumnWidth(
+            Context context,
+            float columnWidthDp)
+    {
+        if (columnWidthDp <= 0) {
+            // Set default columnWidthDp value (48dp here).
+            // It is better to move this constant to static constant on top,
+            // but we need context to convert it to dp, so can't really do so.
+            columnWidthDp = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 48,
+                    context.getResources().getDisplayMetrics());
+        }
+        return columnWidthDp;
+    }
+
+    public void setColumnWidth(float newColumnWidthDp)
+    {
+        if (newColumnWidthDp > 0 && newColumnWidthDp != mColumnWidth) {
+            mColumnWidth = newColumnWidthDp;
+            mColumnWidthChanged = true;
+        }
+    }
+
+    // http://stackoverflow.com/a/42241730
+    @Override
+    public void onLayoutChildren(
+            RecyclerView.Recycler recycler,
+            RecyclerView.State state)
+    {
+        int width = getWidth();
+        int height = getHeight();
+
+        if (width != mWidth) {
+            mWidthChanged = true;
+            mWidth = width;
+        }
+
+        if ((mWidthChanged || mColumnWidthChanged) && mColumnWidth > 0 && width > 0 && height > 0) {
+            int totalSpace;
+            if (getOrientation() == VERTICAL) {
+                totalSpace = width - getPaddingRight() - getPaddingLeft();
+            } else {
+                totalSpace = height - getPaddingTop() - getPaddingBottom();
+            }
+            int spanCount = Math.max(1, (int) Math.floor(totalSpace / mColumnWidth));
+            setSpanCount(spanCount);
+            mColumnWidthChanged = false;
+            mWidthChanged = false;
+        }
+        super.onLayoutChildren(recycler, state);
+    }
+}
