3021f83cd1511d43995dfdf08feef4a742902634
Stanislav Petryakov <become.glory@yandex.ru>
2020-01-10 09:51:04 +0300

    Add download tiles for TMS and update version

--- src/main/java/com/nextgis/maplibui/util/NotificationHelper.java
+++ src/main/java/com/nextgis/maplibui/util/NotificationHelper.java
@@ -3,7 +3,7 @@
  * Purpose:  Mobile GIS for Android.
  * Author:   Stanislav Petriakov, becomeglory@gmail.com
  * *****************************************************************************
- * Copyright (c) 2015-2016, 2018 NextGIS, info@nextgis.com
+ * Copyright (c) 2015-2016, 2018, 2020 NextGIS, info@nextgis.com
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -125,6 +125,7 @@ public final class NotificationHelper {
         NotificationChannel chan = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_HIGH);
         chan.setLightColor(Color.CYAN);
         chan.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+        chan.setSound(null, null);
         NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         assert manager != null;
         manager.createNotificationChannel(chan);
